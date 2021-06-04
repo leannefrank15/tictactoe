e30564bf3a0122197745633594552f7ded95e815
leannefrank <leannfrank@gmail.com>
2021-06-04 13:45:47 +0530

    scss styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Board from "./components/Board"
+import "./styles/root.scss"
 
 export default () => (
-  <div>
-    <h1>TIC TAC TOE!</h1>
+  <div className='app'>
+    <h1 className='headerfull'>TIC <span className='header'>TAC</span> TOE!</h1>
     <Board />
   </div>
 );
